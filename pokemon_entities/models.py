dd3e75d91f83577a59369fa933e40872ab4a50da
Victor Abdukarimov <viredmi@gmail.com>
2022-07-12 12:06:12 +0300

    Added verbose_name in models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,26 +1,35 @@
 from django.db import models
-from django.db.models.deletion import PROTECT
 
 
 
 class Pokemon(models.Model):
+    """Вид покемона"""
     title_ru = models.CharField(max_length=200)
     title_en = models.CharField(max_length=200, blank=True)
     title_jp = models.CharField(max_length=200, blank=True)
     image = models.ImageField(null=True) 
     description = models.TextField(blank=True) 
     evolution = models.ForeignKey(
-        "self", 
-        related_name = "evolutions",
+        "self",
+        verbose_name="Эволюция покемона", 
+        related_name="evolutions",
         on_delete=models.CASCADE,
         blank=True,
         null=True,
     )
+    
     def __str__(self):
         return f"{self.title_ru}"
 
+
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, blank=True)
+    """Рокемон на карте"""
+    pokemon = models.ForeignKey(
+        Pokemon,
+        verbose_name="Вид покемона",
+        on_delete=models.CASCADE, 
+        blank=True
+    )
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField(default=None)
