63d725e0c2922a476abdb22720b7469a003e2aa7
Victor Abdukarimov <viredmi@gmail.com>
2022-07-16 01:47:23 +0300

    Fixed related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -47,7 +47,7 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon,
         verbose_name="Вид покемона",
-        related_name="pokemons",
+        related_name="pokemon_entities",
         on_delete=models.CASCADE,
     )
     lat = models.FloatField(verbose_name='Широта')
